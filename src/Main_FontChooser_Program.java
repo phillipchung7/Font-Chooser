049cf787a6c8cc595e971498d65c4a7f1c6cf6dd
Phillip Chung <pchung@deerfield.edu>
2016-01-29 11:32:19 -0500

    Bonus done!

--- src/Main_FontChooser_Program.java
+++ src/Main_FontChooser_Program.java
@@ -9,15 +9,14 @@ public class Main_FontChooser_Program {
 	JLabel font_type_label = new JLabel ("Font Type");
 	GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
 	String[] names = ge.getAvailableFontFamilyNames();
-	JList nameslist = new JList(names);  	          
+	JList nameslist = new JList(names);  
 	JScrollPane names_listScroller = new JScrollPane(nameslist);
 	JLabel font_size_label = new JLabel ("Font Size");
-	String[] fontsize = {"6", "8", "10", "12", "14", "16", "18", "20","22", "24", "26", "28", "30", "32", "34"};
-	JList fontsizelist = new JList(fontsize);  
+	String[] fontsize = {"4","6", "8", "10", "12", "14", "16", "18", "20", "22", "24"};
+	JList fontsizelist = new JList(fontsize);
 	JScrollPane fontsize_listScroller = new JScrollPane(fontsizelist);	
 	JCheckBox bold = new JCheckBox("Bold");
     JCheckBox italic = new JCheckBox("Italic");
-    JCheckBox underline = new JCheckBox("Underline");
 		
 	JPanel font_color_Pane = new JPanel();
 		JColorChooser font_color_chooser = new JColorChooser();
@@ -35,38 +34,36 @@ public Main_FontChooser_Program() {
 public void frame() {
 	
 	JFrame frame = new JFrame();
-	frame.setVisible(true); // The pop up window is now visible.
-	frame.setSize(710,400); // The pop up window has these dimensions.
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //The pop up window can be closed now by clicking close.
-	frame.setResizable(false);
-	frame.setTitle("Editor");
-			
+	
+	nameslist.setCellRenderer(new FontCellRenderer());
+	
 		Box left1 = Box.createVerticalBox();
 	    	left1.add(font_type_label);
-	    	nameslist.setVisibleRowCount(17);
+	    	names_listScroller.setPreferredSize(new Dimension(350,0));
+	    	left1.add(Box.createVerticalStrut(5));
 	    	left1.add(names_listScroller);
-	    	left1.setMaximumSize(new Dimension(0,300));
-		Box center1 = Box.createVerticalBox();
-			center1.add(font_size_label);
-			center1.add(fontsize_listScroller);
-			center1.setMaximumSize(new Dimension(0,300));
+	    	left1.add(Box.createVerticalStrut(15));
 		Box right1 = Box.createVerticalBox();
+			fontsize_listScroller.setPreferredSize(new Dimension(0,0));			
+			right1.add(font_size_label);
+			right1.add(fontsize_listScroller);
+			right1.add(Box.createVerticalStrut(30));
 			right1.add(bold);
+			bold.setFont(new java.awt.Font("Default", Font.BOLD,18));
 			right1.add(Box.createVerticalStrut(30));
 			right1.add(italic);
-			right1.add(Box.createVerticalStrut(30));
-			right1.add(underline);
+			italic.setFont(new java.awt.Font("Default", Font.ITALIC,18));
+			right1.add(Box.createGlue());
 		Box top1 = Box.createHorizontalBox();
-			top1.add(Box.createGlue());
+			top1.add(Box.createHorizontalStrut(20));
 			top1.add(left1);
-			top1.add(Box.createGlue());
-			top1.add(center1);
 			top1.add(Box.createGlue());
 			top1.add(right1);
 			top1.add(Box.createGlue());
 
-		JTabbedPane jtp = new JTabbedPane();
-		jtp.addTab("Font Style", top1);
+	JTabbedPane jtp = new JTabbedPane();
+	jtp.setFont(new java.awt.Font("Default", Font.ITALIC,16));			
+	jtp.addTab("Font Style", top1);
 
 
 	jtp.addTab("Font Color", font_color_Pane);
@@ -79,11 +76,29 @@ public void frame() {
     	background_color_Pane.add(background_color_chooser);
 
     frame.add(jtp); // Now we add the panel to the frame.
+    frame.setVisible(true); // The pop up window is now visible.
+	frame.setSize(710,400); // The pop up window has these dimensions.
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //The pop up window can be closed now by clicking close.
+	frame.setResizable(false);
+	frame.setTitle("Editor");
+}
 
 
 
-    
-    	
+class FontCellRenderer extends DefaultListCellRenderer {
+
+    public Component getListCellRendererComponent(
+        JList list,
+        Object value,
+        int index,
+        boolean isSelected,
+        boolean cellHasFocus) {
+        JLabel label = (JLabel)super.getListCellRendererComponent(
+            list,value,index,isSelected,cellHasFocus);
+        Font font = new Font((String)value, Font.PLAIN, 16);
+        label.setFont(font);
+        return label;
+    }
 }
 	
 
