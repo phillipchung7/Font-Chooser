12a62a11f3fde9ea4ae7331541608858e6a4021e
Phillip Chung <pchung@deerfield.edu>
2016-01-30 22:41:48 -0500

    Listeners added. Just bold and italic

--- src/Main_FontChooser_Program.java
+++ src/Main_FontChooser_Program.java
@@ -1,7 +1,11 @@
 import javax.swing.*;
+import javax.swing.colorchooser.ColorSelectionModel;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.event.ChangeEvent;
 import java.awt.*;
 import java.util.Hashtable;
-
 
 public class Main_FontChooser_Program {
 	
@@ -9,24 +13,40 @@ public class Main_FontChooser_Program {
 	GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
 	String[] names = ge.getAvailableFontFamilyNames();
 	JList nameslist = new JList(names);  
-		JScrollPane names_listScroller = new JScrollPane(nameslist);
+	JScrollPane names_listScroller = new JScrollPane(nameslist);
 	JLabel font_size_label = new JLabel ("Font Size");
-	JSlider font_slider = new JSlider(5, 25, 15);
+	JSlider font_slider = new JSlider(5, 35, 20);
     JCheckBox bold = new JCheckBox("Bold");
     JCheckBox italic = new JCheckBox("Italic");
 		
 	JPanel font_color_Pane = new JPanel();
-		JColorChooser font_color_chooser = new JColorChooser();
+	JColorChooser font_color_chooser = new JColorChooser();
 	
 	JPanel background_color_Pane = new JPanel();
-		JColorChooser background_color_chooser = new JColorChooser();
+	JColorChooser background_color_chooser = new JColorChooser();
 
+	JPanel preview_Panel = new JPanel(new BorderLayout());
+ 
+	String preview_text = "<html>"
+			+ "The quick brown fox jumped over the lazy dog’s back.<br>"
+			+ "Pack my box with five dozen liquor jugs.<br>"
+			+ "Jackdaws love my big sphinx of quartz.<br>"
+			+ "Mr. Jock, TV quiz PhD, bags few lynx.<br>"
+			+ "abcdefghijklmnopqrstuvwxyz<br>"
+			+ "ABCDEFGHIJKLMNOPQRSTUVWXYZ<br>"
+			+ "01234567890<br>"
+			+ "€†™´¸¢©¤°÷½¼¾>¡¿«‘’<¯µ ·¬ªº¶±£\"»®§­¹²³ß×™¨¥<br>"
+			+ "ÀÁÂÃÄÅÆÇÈÉ ÊËÌÍÎÏÐÑÒÓÔ ÕÖØÙÚÛÜÝÞÿ<br>"
+			+ "àáâãäåæçèé êëìíîïðñòóô õöøùúûüýþÿ<br>"
+			+ "!\"#$%&\'()*+,-./:;<=>?@[\\^_z{|}~<br>"
+			+ "uvw wW gq9 2z 5s il17|!j oO08 `'\" ;:,. m nn rn {[()]}u<br>"
+			+ "</html>";
+	JLabel preview_font = new JLabel (preview_text,JLabel.CENTER);
 
-	
 public Main_FontChooser_Program() {
 		GUI(); 
 	}
-	
+
 public void GUI() {
 	
 	JFrame frame = new JFrame();
@@ -51,6 +71,8 @@ public void GUI() {
 			    table.put (15, new JLabel("15"));
 			    table.put (20, new JLabel("20"));
 			    table.put (25, new JLabel("25"));
+			    table.put (30, new JLabel("30"));
+			    table.put (35, new JLabel("35"));
 			    font_slider.setLabelTable (table);
 			    font_slider.setPaintLabels(true);
 			right1.add(Box.createGlue());
@@ -68,7 +90,7 @@ public void GUI() {
 			top1.add(Box.createGlue());
 			
 	JTabbedPane jtp = new JTabbedPane();
-	jtp.setFont(new java.awt.Font("Default", Font.BOLD,16));			
+	jtp.setFont(new java.awt.Font("Calibri", Font.BOLD,16));			
 	jtp.addTab("Font Style", top1);
 	jtp.addTab("Font Color", font_color_Pane);
 		font_color_chooser.setPreviewPanel(font_color_Pane);
@@ -77,13 +99,60 @@ public void GUI() {
     	background_color_chooser.setPreviewPanel(background_color_Pane);
     	background_color_Pane.add(background_color_chooser);
 
-    frame.add(jtp); // Now we add the panel to the frame.
-    frame.setVisible(true); // The pop up window is now visible.
-	frame.setSize(710,400); // The pop up window has these dimensions.
-	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //The pop up window can be closed now by clicking close.
+    frame.add(jtp); 
+    frame.setVisible(true);
+	frame.setSize(710,400); 
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
 	frame.setResizable(false);
 	frame.setTitle("Editor");
+	
+	JFrame preview = new JFrame();
+	preview.setVisible(true); 
+	preview.setSize(550,500); 
+	preview.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+	preview.setTitle("Preview");
+	
+	preview.add(preview_Panel);
+	preview_Panel.setBackground(Color.WHITE);
+    preview_font.setFont(new Font("Default",Font.PLAIN,20));		
+	preview_Panel.add(preview_font);
 
+
+	
+	nameslist.addListSelectionListener(new ListSelectionListener() {
+	      public void valueChanged(ListSelectionEvent e) {
+	    	  int size = font_slider.getValue();
+	    	  String font_selected = nameslist.getSelectedValue().toString();
+	    	  preview_font.setFont(new Font(font_selected,Font.PLAIN, size));
+	      }
+	    });
+	
+	
+	font_slider.addChangeListener(new ChangeListener() {
+	      public void stateChanged(ChangeEvent evt) {
+	        JSlider font_slider = (JSlider) evt.getSource();
+	        if (font_slider.getValueIsAdjusting()) {
+	          float value = font_slider.getValue();
+	          preview_font.setFont(preview_font.getFont().deriveFont(value));	        }
+	        }
+	    });
+	
+    ColorSelectionModel background_model = background_color_chooser.getSelectionModel();
+    ChangeListener backgroundchangeListener = new ChangeListener() {
+      public void stateChanged(ChangeEvent changeEvent) {
+        Color newBackgroundcolor = background_color_chooser.getColor();
+        preview_Panel.setBackground(newBackgroundcolor);
+      }
+    };
+    background_model.addChangeListener(backgroundchangeListener);
+    
+    ColorSelectionModel font_model = font_color_chooser.getSelectionModel();
+    ChangeListener fontchangeListener = new ChangeListener() {
+      public void stateChanged(ChangeEvent changeEvent) {
+        Color newfontcolor = font_color_chooser.getColor();
+        preview_font.setForeground(newfontcolor);      }
+    };
+    font_model.addChangeListener(fontchangeListener);
 }
 
 class FontCellRenderer extends DefaultListCellRenderer {
